92726f3f1984898139408b6a31b003745b0d7d14
emilianogastaldo <emiliano0303@gmail.com>
2023-12-22 17:26:57 +0100

    tuttifrutti added

--- array_lista/js/script.js
+++ array_lista/js/script.js
@@ -1,5 +1,23 @@
 console.log('js ok')
 
+//Creo il frigorifero con la frutta
 const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
 
-console.tanble(frigorifero);
+console.table(frigorifero);
+
+//aggiungo la pesca
+frigorifero.push('pesca');
+
+console.table(frigorifero);
+
+let message = '';
+//Cerco il cocomero
+const cocomero = 'cocomero'
+for (i = 0; i < frigorifero.length; i++) {
+    message = 'Oh no, devo uscire a comprare il cocomero!'
+    if (frigorifero[i] === cocomero) {
+        message = 'Trovato! Devo solo preparare il cocktail.'
+    }
+}
+
+console.log(message);

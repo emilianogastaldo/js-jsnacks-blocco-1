1e5ca332aea32854cf9242246f93456ba46c48f1
emilianogastaldo <emiliano0303@gmail.com>
2024-01-08 11:59:34 +0100

    usato while invece del for

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -8,17 +8,42 @@ const arrayTwo = [23, 3, 786, 863, 45];
 console.log(arrayOne);
 console.log(arrayTwo);
 
-if (arrayOne.length > arrayTwo.length) {
-    for (i = arrayTwo.length; i < arrayOne.length; i++) {
-        const randomNum = Math.floor(Math.random() * 100) + 1;
-        arrayTwo.push(randomNum);
-    }
-} else {
-    for (i = arrayOne.length; i < arrayTwo.length; i++) {
-        const randomNum = Math.floor(Math.random() * 100) + 1;
-        arrayOne.push(randomNum);
-    }
+// if (arrayOne.length > arrayTwo.length) {
+//     for (i = arrayTwo.length; i < arrayOne.length; i++) {
+//         const randomNum = Math.floor(Math.random() * 100) + 1;
+//         arrayTwo.push(randomNum);
+//     }
+// } else {
+//     for (i = arrayOne.length; i < arrayTwo.length; i++) {
+//         const randomNum = Math.floor(Math.random() * 100) + 1;
+//         arrayOne.push(randomNum);
+//     }
 
+// }
+// console.log(arrayOne);
+// console.log(arrayTwo);
+
+let shorter;
+let bigger;
+
+if (arrayOne.length > arrayTwo.length) {
+    shorter = arrayTwo;
+    bigger = arrayOne;
+} else {
+    bigger = arrayOne;
+    shorter = arrayTwo;
 }
-console.log(arrayOne);
-console.log(arrayTwo);
+
+// const difference = bigger.length - shorter.length;
+
+// for (let i = 0; i < difference; i++) {
+//     const randomNum = Math.floor(Math.random() * 100) + 1;
+//     shorter.push(randomNum);
+// }
+// console.log(shorter, bigger);
+
+while (bigger.length > shorter.length) {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    shorter.push(randomNum);
+}
+console.log(shorter, bigger);
